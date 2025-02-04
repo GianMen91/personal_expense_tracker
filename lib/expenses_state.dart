726ebdc47de7e067f11fccac3feced0153fc1366
GianMen91 <giancarlo.mennillo@gmail.com>
2025-02-04 17:33:05 +0100

    add expenses_state.dart

---
+++ lib/expenses_state.dart
@@ -0,0 +1,11 @@
+import 'expense.dart';
+
+class ExpensesState {
+  final List<Expense> expense;
+  final bool isLoading;
+
+  ExpensesState({
+    required this.expense,
+    this.isLoading = false,
+  });
+}
