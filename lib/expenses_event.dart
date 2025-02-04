6a650d31b6cb0abe0acd78f957adb911a0f61741
GianMen91 <giancarlo.mennillo@gmail.com>
2025-02-04 17:34:18 +0100

    add expense_event

---
+++ lib/expenses_event.dart
@@ -0,0 +1,9 @@
+import 'expense.dart';
+
+abstract class ExpensesEvent {}
+
+class LoadExpense extends ExpensesEvent {}
+class AddExpense extends ExpensesEvent {
+  final Expense expense;
+  AddExpense(this.expense);
+}
