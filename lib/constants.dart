63fe368993f3865801fc14c6d73fd26b9bb85539
GianMen91 <giancarlo.mennillo@gmail.com>
2025-02-05 11:15:17 +0100

    add a separate class for constant

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+// Import the Flutter material library
+import 'package:flutter/material.dart';
+
+// Define color constants
+const kThemeColor = Color(0xFF42224A);
+const kBottomNavigationBarColor = Color(0xFFECEBED);
+const kButtonColor = Color(0xFFBD9DC5);
+
