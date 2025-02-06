5c329f1defceef58431174cca419010253e5edff
GianMen91 <giancarlo.mennillo@gmail.com>
2025-02-06 13:15:18 +0100

    refactor code so to use navigation bloc

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,101 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:personal_expense_tracker/constants.dart';
 import 'package:personal_expense_tracker/models/expense_category.dart';
+import 'package:personal_expense_tracker/models/expense_categories.dart';
 import 'package:personal_expense_tracker/screens/expenses_list_screen.dart';
 import 'package:personal_expense_tracker/screens/statistic_screen.dart';
-
-import '../constants.dart';
-import '../models/expense_categories.dart';
+import '../blocs/navigation/navigation_bloc.dart';
+import '../blocs/navigation/navigation_event.dart';
+import '../blocs/navigation/navigation_state.dart';
 import 'new_expense_screen.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  HomeScreenState createState() => HomeScreenState();
-}
-
-class HomeScreenState extends State<HomeScreen> {
   final PageController _myPage = PageController();
-  int _currentIndex = 0;
-
-  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Personal Expense Tracker', style: TextStyle(fontWeight: FontWeight.bold),),
-        backgroundColor: const Color(0xFFF5F5F5),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kButtonColor,
-        shape: const CircleBorder(),
-        onPressed: () async {
-          await _showMyDialog();
-        },
-        child: const Icon(Icons.add, color: Colors.white, size: 28),
-      ),
-      //floatingActionButton: buildSpeedDial(context),
-      bottomNavigationBar: BottomAppBar(
-        color: kBottomNavigationBarColor,
-        shape: const CircularNotchedRectangle(),
-        child: SizedBox(
-          height: 75,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return BlocBuilder<NavigationBloc, NavigationState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Personal Expense Tracker',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: const Color(0xFFF5F5F5),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: kButtonColor,
+            shape: const CircleBorder(),
+            onPressed: () async {
+              await _showMyDialog(context);
+            },
+            child: const Icon(Icons.add, color: Colors.white, size: 28),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: kBottomNavigationBarColor,
+            shape: const CircularNotchedRectangle(),
+            child: SizedBox(
+              height: 75,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.home,
+                        color: state.currentIndex == 0 ? kThemeColor : Colors.grey),
+                    onPressed: () {
+                      context.read<NavigationBloc>().add(ChangePage(0));
+                      _myPage.jumpToPage(0);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.bar_chart,
+                        color: state.currentIndex == 1 ? kThemeColor : Colors.grey),
+                    onPressed: () {
+                      context.read<NavigationBloc>().add(ChangePage(1));
+                      _myPage.jumpToPage(1);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: PageView(
+            controller: _myPage,
+            onPageChanged: (int index) {
+              context.read<NavigationBloc>().add(ChangePage(index));
+            },
+            physics: const NeverScrollableScrollPhysics(),
             children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.home,
-                    color: _currentIndex == 0 ? kThemeColor : Colors.grey),
-                onPressed: () {
-                  setState(() {
-                    _myPage.jumpToPage(0);
-                    _currentIndex = 0;
-                  });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.bar_chart,
-                    color: _currentIndex == 1 ? kThemeColor : Colors.grey),
-                onPressed: () {
-                  setState(() {
-                    _myPage.jumpToPage(1);
-                    _currentIndex = 1;
-                  });
-                },
-              ),
+              ExpensesListScreen(),
+              StatisticScreen(),
             ],
           ),
-        ),
-      ),
-      body: PageView(
-        controller: _myPage,
-        onPageChanged: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        physics: const NeverScrollableScrollPhysics(),
-        children: <Widget>[
-          ExpensesListScreen(),
-          StatisticScreen(),
-        ],
-      ),
+        );
+      },
     );
   }
 
-  Future<void> _showMyDialog() async {
+  Future<void> _showMyDialog(BuildContext context) async {
     return showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Category'),
+          title: const Text('Category'),
           content: SizedBox(
             height: 300.0,
             width: 300.0,
