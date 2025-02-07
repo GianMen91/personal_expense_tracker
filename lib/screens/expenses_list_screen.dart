48ab72477075e2f3593c20f4485a127e7e724a40
GianMen91 <giancarlo.mennillo@gmail.com>
2025-02-08 00:53:27 +0100

    extract widgets to separate classes

--- lib/screens/expenses_list_screen.dart
+++ lib/screens/expenses_list_screen.dart
@@ -4,8 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../blocs/expense_list/expense_list_bloc.dart';
 import '../blocs/expense_list/expense_list_state.dart';
 
-import '../constants.dart';
 import '../widgets/expense_list.dart';
+import '../widgets/monthly_total_expense_card.dart';
 
 class ExpensesListScreen extends StatelessWidget {
   const ExpensesListScreen({super.key});
@@ -24,40 +24,7 @@ class ExpensesListScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Column(
                 children: [
-                  Container(
-                    width: double.infinity,
-                    margin: const EdgeInsets.symmetric(vertical: 16.0),
-                    padding: const EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                      color: kThemeColor,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 10,
-                          offset: const Offset(0, 5),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 10),
-                        Text(
-                          '${state.monthlyTotal.toStringAsFixed(2)} €',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        const Text('Total expense this month',
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 18)),
-                      ],
-                    ),
-                  ),
+                  MonthlyTotalExpenseCard(monthlyTotal: state.monthlyTotal),
                   Expanded(
                     child: ExpenseList(groupedExpenses: state.groupedExpenses),
                   ),
@@ -68,5 +35,3 @@ class ExpensesListScreen extends StatelessWidget {
         ));
   }
 }
-
-
