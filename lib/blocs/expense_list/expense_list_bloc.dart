faf2a82d4b77345a550a5a96431fd92c89e31a04
GianMen91 <giancarlo.mennillo@gmail.com>
2025-02-10 00:35:46 +0100

    add comments

--- lib/blocs/expense_list/expense_list_bloc.dart
+++ lib/blocs/expense_list/expense_list_bloc.dart
@@ -1,57 +1,75 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../models/expense_data.dart';
-import '../../repositories/database_helper.dart'; // Import your database helper
+import '../../models/expense_data.dart'; // Importing the expense data model
+import '../../repositories/database_helper.dart'; // Importing the database helper to interact with the database
 
-import '../../models/expense.dart';
-import 'package:intl/intl.dart';
+import '../../models/expense.dart'; // Importing the Expense model
+import 'package:intl/intl.dart'; // Importing package for date formatting
 
-import 'expense_list_event.dart';
-import 'expense_list_state.dart';
+import 'expense_list_event.dart'; // Importing the events related to the expense list
+import 'expense_list_state.dart'; // Importing the state of the expense list
 
+// ExpensesListBloc handles the loading, adding, and deleting of expenses in the expense list.
 class ExpensesListBloc extends Bloc<ExpensesListEvent, ExpensesListState> {
-  final DatabaseHelper dbHelper;
+  final DatabaseHelper
+      dbHelper; // Database helper for performing operations on the database
 
+  // Constructor initializes the BLoC and sets up event handlers.
   ExpensesListBloc(this.dbHelper)
       : super(ExpensesListState(expenses: [], isLoading: true)) {
+    // Mapping events to their corresponding handlers.
     on<LoadExpense>(_onLoadExpense);
     on<AddExpense>(_onAddExpense);
     on<DeleteExpense>(_onDeleteExpense);
   }
 
+  // Handler for loading expenses from the database.
   Future<void> _onLoadExpense(
       LoadExpense event, Emitter<ExpensesListState> emit) async {
-    emit(state.copyWith(isLoading: true));
+    emit(state.copyWith(
+        isLoading: true)); // Set loading state to true before fetching data
     try {
-      final expenses = await dbHelper.getExpenses();
+      final expenses =
+          await dbHelper.getExpenses(); // Fetch expenses from the database
+      // Update state with expenses and set loading to false.
       emit(_updateStateWithDerivedData(
           state.copyWith(expenses: expenses, isLoading: false)));
     } on Exception catch (e) {
+      // If an error occurs, update the state with the error message.
       emit(state.copyWith(isLoading: false, errorMessage: e.toString()));
     }
   }
 
+  // Handler for adding a new expense to the database.
   Future<void> _onAddExpense(
       AddExpense event, Emitter<ExpensesListState> emit) async {
     try {
-      await dbHelper.addExpense(event.expense);
-      final expenses = await dbHelper.getExpenses();
+      await dbHelper
+          .addExpense(event.expense); // Add the expense to the database
+      final expenses =
+          await dbHelper.getExpenses(); // Fetch updated list of expenses
       emit(_updateStateWithDerivedData(state.copyWith(expenses: expenses)));
     } on Exception catch (e) {
+      // If an error occurs, update the state with the error message.
       emit(state.copyWith(errorMessage: e.toString()));
     }
   }
 
+  // Handler for deleting an expense from the database.
   Future<void> _onDeleteExpense(
       DeleteExpense event, Emitter<ExpensesListState> emit) async {
     try {
-      await dbHelper.deleteExpense(event.expense.id!);
-      final expenses = await dbHelper.getExpenses();
+      await dbHelper.deleteExpense(
+          event.expense.id!); // Delete the expense from the database
+      final expenses =
+          await dbHelper.getExpenses(); // Fetch updated list of expenses
       emit(_updateStateWithDerivedData(state.copyWith(expenses: expenses)));
     } on Exception catch (e) {
+      // If an error occurs, update the state with the error message.
       emit(state.copyWith(errorMessage: e.toString()));
     }
   }
 
+  // Updates the state with additional derived data like grouped expenses and monthly total.
   ExpensesListState _updateStateWithDerivedData(
       ExpensesListState currentState) {
     final expenseData = _calculateExpenseData(currentState.expenses);
@@ -61,47 +79,63 @@ class ExpensesListBloc extends Bloc<ExpensesListEvent, ExpensesListState> {
     );
   }
 
+  // Calculates grouped expenses and monthly total from the list of expenses.
   ExpenseData _calculateExpenseData(List<Expense> expenses) {
-    final groupedExpenses = _groupExpensesByDate(expenses);
-    final monthlyTotal = _calculateMonthlyTotal(expenses);
+    final groupedExpenses =
+        _groupExpensesByDate(expenses); // Group expenses by date
+    final monthlyTotal = _calculateMonthlyTotal(
+        expenses); // Calculate total cost for the current month
     return ExpenseData(
         groupedExpenses: groupedExpenses,
         monthlyTotal: monthlyTotal,
         expenses: expenses);
   }
 
+  // Groups expenses by date, with the format 'Today', 'dd MMM yyyy', etc.
   Map<String, List<Expense>> _groupExpensesByDate(List<Expense> expenses) {
     final groupedExpenses = <String, List<Expense>>{};
     for (var expense in expenses) {
-      final dateStr = _formatDate(expense.date);
-      groupedExpenses.putIfAbsent(dateStr, () => []).add(expense);
+      final dateStr = _formatDate(expense.date); // Format the date as a string
+      groupedExpenses
+          .putIfAbsent(dateStr, () => [])
+          .add(expense); // Group by formatted date
     }
+    // Sort grouped expenses by date, from most recent to oldest.
     return Map.fromEntries(groupedExpenses.entries.toList()
       ..sort((a, b) => _parseDate(b.key).compareTo(_parseDate(a.key))));
   }
 
+  // Parses a date string to a DateTime object for sorting and comparison.
   DateTime _parseDate(String dateStr) {
     if (dateStr == 'Today') {
-      return DateTime.now();
+      return DateTime.now(); // If the date is 'Today', return current date.
     }
-    return DateFormat('dd MMM yyyy').parse(dateStr);
+    return DateFormat('dd MMM yyyy')
+        .parse(dateStr); // Parse the date string into a DateTime object.
   }
 
+  // Formats a DateTime object into a string with the format 'Today' or 'dd MMM yyyy'.
   String _formatDate(DateTime date) {
     final now = DateTime.now();
     if (date.year == now.year &&
         date.month == now.month &&
         date.day == now.day) {
-      return 'Today';
+      return 'Today'; // Return 'Today' if the date is today's date
     }
-    return DateFormat('dd MMM yyyy').format(date);
+    return DateFormat('dd MMM yyyy')
+        .format(date); // Otherwise, return the formatted date.
   }
 
+  // Calculates the total expenses for the current month.
   double _calculateMonthlyTotal(List<Expense> expenses) {
     final now = DateTime.now();
     return expenses
         .where((expense) =>
-            expense.date.year == now.year && expense.date.month == now.month)
-        .fold(0, (sum, expense) => sum + expense.cost);
+            expense.date.year == now.year &&
+            expense.date.month == now.month) // Filter by current month and year
+        .fold(
+            0,
+            (sum, expense) =>
+                sum + expense.cost); // Calculate the sum of the expenses.
   }
 }
--- lib/blocs/expenses_stat/expenses_stat_bloc.dart
+++ lib/blocs/expenses_stat/expenses_stat_bloc.dart
@@ -1,71 +1,88 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart'; // For date formatting
 
-import '../../models/expense.dart';
-import 'expenses_stat_event.dart';
-import 'expenses_stat_state.dart';
+import '../../models/expense.dart'; // Importing the Expense model
+import 'expenses_stat_event.dart'; // Importing the events related to expenses statistics
+import 'expenses_stat_state.dart'; // Importing the state for expenses statistics
 
+// ExpensesStatBloc handles the logic for filtering and calculating expense statistics.
 class ExpensesStatBloc extends Bloc<ExpensesStatEvent, ExpensesStatState> {
+  // Constructor initializes the BLoC with the current date as the default selected date.
   ExpensesStatBloc() : super(ExpensesStatState(selectedDate: DateTime.now())) {
+    // Mapping events to their corresponding handlers.
     on<ChangeCategoryEvent>(_onChangeCategory);
     on<ChangeYearEvent>(_onChangeYear);
     on<ChangeMonthSelectionEvent>(_onChangeMonthSelection);
   }
 
+  // Handler for when the category is changed in the statistics view.
   void _onChangeCategory(
       ChangeCategoryEvent event, Emitter<ExpensesStatState> emit) {
+    // Update the state with the selected category and keep the selected month as is.
     emit(state.copyWith(
         selectedCategory: event.category, selectedMonth: state.selectedMonth));
   }
 
+  // Handler for when the year is changed (by offset) in the statistics view.
   void _onChangeYear(ChangeYearEvent event, Emitter<ExpensesStatState> emit) {
+    // Calculate the new date based on the offset, changing only the year.
     final newDate = DateTime(
         state.selectedDate.year + event.offset, state.selectedDate.month);
+    // Update the state with the new year and the same month.
     emit(state.copyWith(selectedDate: newDate));
   }
 
+  // Handler for when the month selection is changed.
   void _onChangeMonthSelection(
       ChangeMonthSelectionEvent event, Emitter<ExpensesStatState> emit) {
+    // Update the state with the newly selected month.
     emit(state.copyWith(selectedMonth: event.month));
   }
 
+  // Filters the expenses based on the selected year, month, and category.
   List<Expense> getFilteredExpenses(
       List<Expense> expenses, ExpensesStatState state) {
     final filteredExpenses = expenses.where((expense) {
-      final isSameYear = expense.date.year == state.selectedDate.year;
+      final isSameYear = expense.date.year == state.selectedDate.year; // Match year
       final isSameMonth = state.selectedMonth == null ||
-          DateFormat('MMM').format(expense.date) == state.selectedMonth;
+          DateFormat('MMM').format(expense.date) == state.selectedMonth; // Match month
       final isSameCategory = state.selectedCategory == "ALL" ||
-          expense.category == state.selectedCategory;
+          expense.category == state.selectedCategory; // Match category
       return isSameYear && isSameMonth && isSameCategory;
     }).toList();
 
+    // Sort the filtered expenses by date in descending order (most recent first).
     final sortedExpenses = List<Expense>.from(filteredExpenses);
     sortedExpenses.sort((a, b) => b.date.compareTo(a.date));
 
     return sortedExpenses;
   }
 
+  // Calculates the total cost of the expenses.
   double calculateTotalAmount(List<Expense> expenses) {
+    // Sum all expenses using fold.
     return expenses.fold(0, (sum, expense) => sum + expense.cost);
   }
 
+  // Retrieves the category with the highest spending for the selected month and year.
   String? getHighestSpendingCategory(
       List<Expense> expenses, DateTime selectedDate, String? selectedMonth) {
     final filteredExpenses = expenses.where((expense) {
-      final isSameYear = expense.date.year == selectedDate.year;
+      final isSameYear = expense.date.year == selectedDate.year; // Match year
       final isSameMonth = selectedMonth == null ||
-          DateFormat('MMM').format(expense.date) == selectedMonth;
+          DateFormat('MMM').format(expense.date) == selectedMonth; // Match month
       return isSameYear && isSameMonth;
     }).toList();
 
+    // If no expenses match, return null.
     if (filteredExpenses.isEmpty) return null;
 
     final categoryTotals = <String, double>{};
+    // Calculate the total spending for each category.
     for (final expense in filteredExpenses) {
       categoryTotals.update(
         expense.category,
-        (existingTotal) => existingTotal + expense.cost,
+            (existingTotal) => existingTotal + expense.cost,
         ifAbsent: () => expense.cost,
       );
     }
@@ -73,32 +90,38 @@ class ExpensesStatBloc extends Bloc<ExpensesStatEvent, ExpensesStatState> {
     double highestTotal = 0;
     List<String> highestCategories = [];
 
+    // Find the category with the highest spending.
     categoryTotals.forEach((category, total) {
       if (total > highestTotal) {
         highestTotal = total;
         highestCategories = [category];
       } else if (total == highestTotal) {
-        highestCategories.add(category);
+        highestCategories.add(category); // In case of a tie, add all matching categories
       }
     });
 
+    // Return the category or categories with the highest total.
     return highestCategories.isNotEmpty ? highestCategories.join(', ') : null;
   }
 
+  // Calculates the total amount spent per month for the selected year.
   List<MapEntry<String, double>> getMonthlyData(
       List<Expense> expenses, DateTime selectedDate) {
+    // Initialize a map with months as keys and 0 as the initial value.
     final Map<String, double> monthlyData = {
       for (var i = 1; i <= 12; i++)
         DateFormat('MMM').format(DateTime(selectedDate.year, i)): 0.0
     };
 
+    // Calculate the total spending per month.
     for (var expense in expenses) {
-      if (expense.date.year == selectedDate.year) {
+      if (expense.date.year == selectedDate.year) { // Match the selected year
         final month = DateFormat('MMM').format(expense.date);
         monthlyData[month] = monthlyData[month]! + expense.cost;
       }
     }
 
+    // Return the monthly data sorted by month.
     return monthlyData.entries.toList();
   }
 }
